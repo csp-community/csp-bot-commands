58c1a09075d66d2421fefe0b5cac06cbb34a32b5
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-04-23 16:02:07 -0400

    Add even more fun

--- csp_bot_commands/tests/test_fun.py
+++ csp_bot_commands/tests/test_fun.py
@@ -34,6 +34,26 @@ class TestFun:
             ("nietzsche",),
             ("twain",),
             ("zen",),
+            ("stoic",),
+            ("churchill",),
+            ("confucius",),
+            ("starwars",),
+            ("hitch",),
+            ("hemingway",),
+            ("dad",),
+            ("chuck",),
+            ("soviet",),
+            ("conspiracy",),
+            ("magic8",),
+            ("compliment",),
+            ("latin",),
+            ("pirate",),
+            ("corporate",),
+            ("motivational",),
+            ("haiku",),
+            ("programming",),
+            ("whiskey",),
+            ("highfive",),
         ],
     )
     def test_execute(self, args):
@@ -84,5 +104,45 @@ class TestFun:
             assert msg.msg.startswith("<@123> shares a Mark Twain quip with <@456>:")
         elif args[0] == "zen":
             assert msg.msg.startswith("<@123> offers <@456> a moment of zen:")
+        elif args[0] == "stoic":
+            assert msg.msg.startswith("<@123> offers <@456> some Stoic counsel:")
+        elif args[0] == "churchill":
+            assert msg.msg.startswith("<@123> channels Churchill for <@456>:")
+        elif args[0] == "confucius":
+            assert msg.msg.startswith("<@123> shares ancient wisdom with <@456>:")
+        elif args[0] == "starwars":
+            assert msg.msg.startswith("<@123> reaches for the Force on behalf of <@456>:")
+        elif args[0] == "hitch":
+            assert msg.msg.startswith("<@123> consults the Guide for <@456>:")
+        elif args[0] == "hemingway":
+            assert msg.msg.startswith("<@123> pours a drink and quotes Hemingway for <@456>:")
+        elif args[0] == "dad":
+            assert msg.msg.startswith("<@123> subjects <@456> to a dad joke:")
+        elif args[0] == "chuck":
+            assert msg.msg.startswith("<@123> informs <@456> of a Chuck Norris fact:")
+        elif args[0] == "soviet":
+            assert msg.msg.startswith("<@123> reminds <@456>:")
+        elif args[0] == "conspiracy":
+            assert msg.msg.startswith("<@123> whispers to <@456>:")
+        elif args[0] == "magic8":
+            assert msg.msg.startswith("<@123> shakes the Magic 8-Ball for <@456>:")
+        elif args[0] == "compliment":
+            assert msg.msg.startswith("<@123> lavishes praise on <@456>:")
+        elif args[0] == "latin":
+            assert msg.msg.startswith("<@123> schools <@456> in Latin:")
+        elif args[0] == "pirate":
+            assert msg.msg.startswith("<@123> makes <@456> walk the plank:")
+        elif args[0] == "corporate":
+            assert msg.msg.startswith("<@123> synergizes with <@456>:")
+        elif args[0] == "motivational":
+            assert msg.msg.startswith("<@123> puts a poster on <@456>")
+        elif args[0] == "haiku":
+            assert msg.msg.startswith("<@123> composes a haiku for <@456>:")
+        elif args[0] == "programming":
+            assert msg.msg.startswith("<@123> shares a programming joke with <@456>:")
+        elif args[0] == "whiskey":
+            assert msg.msg.startswith("<@123> calls <@456> over to the")
+        elif args[0] == "highfive":
+            assert msg.msg.startswith("<@123> gives <@456> a magnificent")
         else:
             assert False
